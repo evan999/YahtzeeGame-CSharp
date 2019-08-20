d9cefd0ac2fdef7642f4fd1ac797d150c6778080
evan999 <emeshberge@gmail.com>
2019-08-19 20:15:42 -0400

    Initial commit. Rolls five game dice.

---
+++ YahtzeeGame/Game.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YahtzeeGame
+{
+    class Game
+    {
+        public int RollGameDie()
+        {
+            GameDie die1 = new GameDie();
+            Random randomizer = new Random();
+            int roll = randomizer.Next(1, 6);
+            die1.value = roll;
+            Console.WriteLine("Die: " + die1.value);
+            return roll;
+        }
+
+        
+        public int[] RollGameDice()
+        {
+            int numberOfDice = 5;
+            int[] cup = new int[numberOfDice];
+            Random randomizer = new Random();
+           
+            //var cup = new List<GameDie>();
+
+            for (int dice = 0; dice < numberOfDice; dice++)
+            {
+                cup[dice] = randomizer.Next(1, 7);
+                Console.WriteLine("Die: " + cup[dice].ToString());
+                //string name = "die" + dice.ToString();
+                //cup.Add(new GameDie());    
+            }
+
+            return cup;
+        }        
+    }
+}
--- YahtzeeGame/Program.cs
+++ YahtzeeGame/Program.cs
@@ -10,6 +10,10 @@ namespace YahtzeeGame
     {
         static void Main(string[] args)
         {
+            Game gameSession = new Game();
+            // gameSession.RollGameDie();
+            gameSession.RollGameDice();
+            Console.ReadLine();
         }
     }
 }
